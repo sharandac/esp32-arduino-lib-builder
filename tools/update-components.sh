0c1ab8e367e0106e3abb7556b345f3f15ab45b66
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-03-10 17:02:55 +0100

    DSP enable

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -104,17 +104,17 @@ if [ $? -ne 0 ]; then exit 1; fi
 # CLONE/UPDATE ESP-DSP
 #
 
-#if [ ! -d "$AR_COMPS/esp-dsp" ]; then
-#	git clone $DSP_REPO_URL "$AR_COMPS/esp-dsp"
+if [ ! -d "$AR_COMPS/esp-dsp" ]; then
+	git clone $DSP_REPO_URL "$AR_COMPS/esp-dsp"
 	# cml=`cat "$AR_COMPS/esp-dsp/CMakeLists.txt"`
 	# echo "if(IDF_TARGET STREQUAL \"esp32\" OR IDF_TARGET STREQUAL \"esp32s2\" OR IDF_TARGET STREQUAL \"esp32s3\")" > "$AR_COMPS/esp-dsp/CMakeLists.txt"
 	# echo "$cml" >> "$AR_COMPS/esp-dsp/CMakeLists.txt"
 	# echo "endif()" >> "$AR_COMPS/esp-dsp/CMakeLists.txt"
-#else
-#	git -C "$AR_COMPS/esp-dsp" fetch && \
-#	git -C "$AR_COMPS/esp-dsp" pull --ff-only
-#fi
-#if [ $? -ne 0 ]; then exit 1; fi
+else
+	git -C "$AR_COMPS/esp-dsp" fetch && \
+	git -C "$AR_COMPS/esp-dsp" pull --ff-only
+fi
+if [ $? -ne 0 ]; then exit 1; fi
 
 #
 # CLONE/UPDATE TINYUSB
